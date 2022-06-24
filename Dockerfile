8051b17402082c34d0571fb0b0bcfc3492a1ab4b
Wojciech Bartnik <wojciech.bartnik>
2022-06-24 10:40:16 +0200

    Working on k8s support - IN PROGRESS

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,14 @@
 FROM debian:stable-slim
 
-COPY start_kafka.bash /bin/
-COPY start_kafka_with_first_time_setup.bash /bin/
-
-RUN apt update && apt install wget default-jre vim -y
+RUN apt update && apt install wget default-jre -y
 RUN wget https://dlcdn.apache.org/kafka/3.2.0/kafka_2.13-3.2.0.tgz && \
     tar xvf kafka_2.13-3.2.0.tgz -C /opt && \
     mv /opt/kafka_2.13-3.2.0 /opt/kafka
+
+EXPOSE 9092
+
+COPY start_kafka.bash /bin/
+COPY start_kafka_with_first_time_setup.bash /bin/
 
 WORKDIR /opt/kafka
 CMD ["/bin/start_kafka.bash"]
--- start_kafka.bash
+++ start_kafka.bash
@@ -7,15 +7,36 @@ set -m
 CLUSTER_ID=$(/opt/kafka/bin/kafka-storage.sh random-uuid)
 /opt/kafka/bin/kafka-storage.sh format -t $CLUSTER_ID -c /opt/kafka/config/kraft/server.properties
 
+# Set Kafka options
+echo "listeners=$KAFKA_LISTENERS" >> /opt/kafka/config/kraft/server.properties
+echo "advertised.listeners=$KAFKA_ADVERTISED_LISTENERS" >> /opt/kafka/config/kraft/server.properties
+echo "listener.security.protocol.map=$KAFKA_LISTENER_SECURITY_PROTOCOL_MAP" >> /opt/kafka/config/kraft/server.properties
+echo "controller.listener.names=$KAFKA_CONTROLLER_LISTENER" >> /opt/kafka/config/kraft/server.properties
+echo "inter.broker.listener.name=$KAFKA_INTER_BROKER_LISTENER_NAME" >> /opt/kafka/config/kraft/server.properties
+
+
+
+echo "Kafka configuration" # without comments and newlines
+echo ""
+grep -v "^#" /opt/kafka/config/kraft/server.properties | grep -v "^$"
+echo ""
+
 echo "Starting Kafka"
 # First one runs in background
 /opt/kafka/bin/kafka-server-start.sh /opt/kafka/config/kraft/server.properties &
 
 # Second one will create topics and close. Then return first one to foreground for SIGINT|SIGTERM
-export IFS=","
-for TOPIC in $KAFKA_CREATE_TOPICS; do
-    until echo "Trying to create topic $TOPIC" && /opt/kafka/bin/kafka-topics.sh --create --topic $TOPIC --partitions 1 --replication-factor 1 --bootstrap-server localhost:9092; do
-        echo "Failed to create topic $TOPIC. Retry"
-    done
+# TODO: parse partitions and replication factors from ENV
+IFS=,
+for TOPIC in $KAFKA_CREATE_TOPICS
+do
+    echo "Trying to create topic $TOPIC" && \
+    /opt/kafka/bin/kafka-topics.sh \
+    --create \
+    --topic $TOPIC \
+    --partitions 10 \
+    --replication-factor 1 \
+    --bootstrap-server localhost:9092 &
 done
-fg
+
+fg %/opt/kafka/bin/kafka-server-start.sh
